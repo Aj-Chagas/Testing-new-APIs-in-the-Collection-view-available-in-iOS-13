83964169f26ae761cbb85bbc7983076fef80a5a9
Anderson Chagas <anderson.chagas@luizalabs.com>
2023-02-23 15:45:17 -0300

    remove main storyboard and set up project to work with view code

---
+++ CollectionViewiOS13/CollectionViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
